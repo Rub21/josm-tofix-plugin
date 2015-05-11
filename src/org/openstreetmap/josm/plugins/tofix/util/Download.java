b83e3665fd246a1b7ba337ffc5330220dfa7ff4f
Rub21 <ruben@mapbox.com>
2015-05-11 13:37:43 +0530

    select object after download data

--- src/org/openstreetmap/josm/plugins/tofix/util/Download.java
+++ src/org/openstreetmap/josm/plugins/tofix/util/Download.java
@@ -1,17 +1,20 @@
 package org.openstreetmap.josm.plugins.tofix.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.actions.downloadtasks.DownloadOsmTask;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.osm.Node;
-import org.openstreetmap.josm.plugins.tofix.TofixDialog;
-import org.openstreetmap.josm.actions.downloadtasks.DownloadOsmTask;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
-import org.openstreetmap.josm.plugins.tofix.bean.ItemUnconnectedBean;
+import org.openstreetmap.josm.plugins.tofix.TofixDialog;
 
 /**
  *
@@ -21,7 +24,7 @@ public class Download {
 
     private static Future<?> future;
 
-    public static void Download(DownloadOsmTask task, Bounds bounds, final Long osm_obj_id) {
+    public static void Download(final DownloadOsmTask task, Bounds bounds, final Long osm_obj_id) {
         ProgressMonitor monitor = null;
         final Future<?> future = task.download(true, bounds, monitor);
         Runnable runAfterTask = new Runnable() {
@@ -29,11 +32,15 @@ public class Download {
             @Override
             public void run() {
                 try {
-                    future.get();                    
+                    future.get();
                     Node node = new Node(osm_obj_id);
-                    //Collection<OsmPrimitive> selection = task.getDownloadedData().allPrimitives();
-                    Main.main.getCurrentDataSet().setSelected(node);
-
+                    Relation relation = new Relation(osm_obj_id);
+                    Way way = new Way(osm_obj_id);
+                    List<OsmPrimitive> selection = new ArrayList<>();
+                    selection.add(way);
+                    selection.add(node);
+                    selection.add(relation);
+                    Main.main.getCurrentDataSet().setSelected(selection);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(TofixDialog.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (ExecutionException ex) {
