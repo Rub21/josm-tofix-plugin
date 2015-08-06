4bc3b9fc142a6e993f3091c9d53688868b72db02
Rub21 <ruben@mapbox.com>
2015-08-06 11:17:15 -0500

    new server

--- src/org/openstreetmap/josm/plugins/tofix/util/Config.java
+++ src/org/openstreetmap/josm/plugins/tofix/util/Config.java
@@ -1,7 +1,7 @@
 package org.openstreetmap.josm.plugins.tofix.util;
 
 public class Config {
-    public static final String host = "http://54.165.131.155:8000/";
+    public static final String host = "http://52.3.92.224:8000/";
     public static final String url_tofix = "http://osmlab.github.io/to-fix/";
     public static final String url_osm = "http://www.openstreetmap.org";    
 }
