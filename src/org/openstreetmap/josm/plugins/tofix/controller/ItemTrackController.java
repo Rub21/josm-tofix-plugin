26f33554eb866837036f5a308f980f34280e3a10
Rub21 <ruben@mapbox.com>
2015-10-20 11:15:11 -0500

    using jsonp instead of gson

--- src/org/openstreetmap/josm/plugins/tofix/controller/ItemTrackController.java
+++ src/org/openstreetmap/josm/plugins/tofix/controller/ItemTrackController.java
@@ -1,52 +1,72 @@
 package org.openstreetmap.josm.plugins.tofix.controller;
 
-import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import org.openstreetmap.josm.plugins.tofix.bean.FixedBean;
 import org.openstreetmap.josm.plugins.tofix.bean.TrackBean;
 import org.openstreetmap.josm.plugins.tofix.util.Request;
 
 public class ItemTrackController {
+
     public void send_track_edit(String url, TrackBean trackBean) {
-        Gson gson = new Gson();
-        String string_obj = gson.toJson(trackBean);
+        JsonObjectBuilder attributesBuilder = Json.createObjectBuilder();
+        JsonObjectBuilder trackBeanBuilder = Json.createObjectBuilder();
+        trackBeanBuilder.add("user", trackBean.getAttributes().getUser())
+                .add("action", trackBean.getAttributes().getAction())
+                .add("key", trackBean.getAttributes().getKey())
+                .add("editor", trackBean.getAttributes().getEditor());
+        attributesBuilder.add("attributes", trackBeanBuilder);
+        JsonObject track_edit = attributesBuilder.build();
         try {
-            Request.sendPOST_Json(url, string_obj);
+            Request.sendPOST_Json(url, track_edit.toString());
         } catch (IOException ex) {
             Logger.getLogger(ItemTrackController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public void send_track_skip(String url, TrackBean trackBean) {
-        Gson gson = new Gson();
-        String string_obj = gson.toJson(trackBean);
+        JsonObjectBuilder attributesBuilder = Json.createObjectBuilder();
+        JsonObjectBuilder trackBeanBuilder = Json.createObjectBuilder();
+        trackBeanBuilder.add("user", trackBean.getAttributes().getUser())
+                .add("action", trackBean.getAttributes().getAction())
+                .add("key", trackBean.getAttributes().getKey())
+                .add("editor", trackBean.getAttributes().getEditor());
+        attributesBuilder.add("attributes", trackBeanBuilder);
+        JsonObject track_skip = attributesBuilder.build();
         try {
-            Request.sendPOST_Json(url, string_obj);
+            Request.sendPOST_Json(url, track_skip.toString());
         } catch (IOException ex) {
             Logger.getLogger(ItemTrackController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public void send_track_fix(String url, FixedBean fixedBean) {
-        Gson gson = new Gson();
-        String string_obj = gson.toJson(fixedBean);
+        JsonObjectBuilder fixedBeanBeanBuilder = Json.createObjectBuilder();
+        fixedBeanBeanBuilder.add("user", fixedBean.getUser())
+                .add("key", fixedBean.getKey());
+        JsonObject track_fixed = fixedBeanBeanBuilder.build();
         try {
-            Request.sendPOST_Json(url, string_obj);
+            Request.sendPOST_Json(url, track_fixed.toString());
         } catch (IOException ex) {
             Logger.getLogger(ItemTrackController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void send_track_noterror(String url, FixedBean NoterrorBean) {
-        Gson gson = new Gson();
-        String string_obj = gson.toJson(NoterrorBean);
+    public void send_track_noterror(String url, FixedBean noterrorBean) { //noterrorBean has the same structure to  FixedBean
+        JsonObjectBuilder noterrorBeanBeanBuilder = Json.createObjectBuilder();
+        noterrorBeanBeanBuilder.add("user", noterrorBean.getUser())
+                .add("key", noterrorBean.getKey());
+        JsonObject track_noterror = noterrorBeanBeanBuilder.build();
         try {
-            Request.sendPOST_Json(url, string_obj);
+            Request.sendPOST_Json(url, track_noterror.toString());
         } catch (IOException ex) {
             Logger.getLogger(ItemTrackController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
+
 }
